a5379704cb5c16d2b69e1abf747d49a9dea76725
Michael Neumann <mneumann@ntecs.de>
2008-01-29 17:22:15 +0100

    Initial creation of HttpUrl class and test cases.

---
+++ http_url.rb
@@ -0,0 +1,140 @@
+#
+# Code for parsing/normalizing URLs 
+#
+# TODO: resolve . and ..
+#
+# Copyright (c) 2008 by Michael Neumann (mneumann@ntecs.de)
+#
+class HttpUrl
+  attr_accessor :host, :port, :path, :query, :original_url
+
+  require 'uri'
+  require 'digest/sha1'
+
+  def self.parse(href, base_url=nil)
+    return nil if href.nil?
+
+    url = self.new
+    url.original_url = href 
+
+    href = href.dup
+    href.strip!
+
+    href.tr!(" \t\r\n", "++++") # escape whitespace
+
+    begin
+      uri = URI.parse(href)
+    rescue URI::Error
+      return nil
+    end
+
+    # 
+    # We only handle http URLs, no https or other.
+    #
+    return nil if uri.scheme and uri.scheme !~ /^http$/i
+
+    #
+    # Host
+    #
+    url.host = uri.host
+    if url.host.nil?
+      # 
+      # If no host is specified, we have to use the host
+      # of <tt>base_url</tt> if given.
+      #
+      return nil if base_url.nil?
+      url.host = base_url.host
+    end
+    return nil if url.host.nil?
+    url.host = url.host.downcase
+
+    #
+    # Port
+    #
+    url.port = uri.port
+    if url.port.nil?
+      return nil if base_url.nil?
+      url.port = base_url.port
+    end
+
+    #
+    # Path
+    #
+    path = uri.path 
+    return nil if path.nil? # it's an invalid URL!
+    if path.empty?
+
+      #
+      # http://www.ntecs.de # => scheme="http", path=""
+      # ?query              # => scheme=nil, path=""
+      #
+      if uri.scheme
+        path = "/"
+      else
+        # relative URL
+        return nil if base_url.nil?
+        path = base_url.path 
+      end
+    elsif path !~ /^\//
+      # relative path
+      return nil if base_url.nil?
+      return nil if base_url.path.nil?
+      return nil if base_url.path !~ /^\// 
+      path = base_url.path[0..(base_url.path.rindex("/"))] + path
+    else
+      # absolute path
+    end
+
+    url.path = path
+
+    #
+    # Query (normalize)
+    #
+    if query = uri.query
+      begin
+        params = CGI.parse(query) 
+        new_query = []
+        params.keys.sort.each do |k|
+          params[k].sort.each do |v|
+            if v
+              new_query << "#{CGI.escape(k)}=#{CGI.escape(v)}" 
+            else
+              new_query << CGI.escape(k)
+            end
+          end
+        end
+        query = new_query.join("&")
+      rescue Exception 
+      end
+    end
+    url.query = query
+
+    #
+    # Assert
+    #
+    raise if url.host.nil? or url.port.nil? or url.path.nil? or url.path.empty? or url.path !~ /^\//
+
+    return url
+  rescue
+    nil
+  end
+
+  def to_filename
+    a = @host.split(".").reverse.map {|s| s.gsub!(/[^A-Za-z0-9_-]*/, '') }
+    a << Digest::SHA1.hexdigest(self.to_s)
+    a.join('/')
+  end
+
+  def to_a
+    [@host, @port, @path, @query]
+  end
+
+  def to_s
+    str = "http://#{@host}:#{@port}#{@path}"
+    if @query
+      str << "?"
+      str << @query
+    end
+    str
+  end
+end
---
+++ test_http_url.rb
@@ -0,0 +1,47 @@
+#
+# Copyright (c) 2008 by Michael Neumann (mneumann@ntecs.de)
+#
+
+require 'test/unit'
+require 'http_url'
+
+class Test_HtmlUrl < Test::Unit::TestCase
+  def test_relative_without_base_url
+    assert_nil parse("a/b/c")
+    assert_nil parse("/a/b/c")
+    assert_nil parse("?abc")
+    assert_nil parse("/test?abc")
+    assert_nil parse("http:test")
+    assert_nil parse("http:/test")
+  end
+
+  def test_absolute
+    assert_equal ['www.ntecs.de', 80, '/', nil], parse('http://www.ntecs.de').to_a
+    assert_equal ['www.ntecs.de', 80, '/', nil], parse('http://www.ntecs.de:80').to_a
+    assert_equal ['www.ntecs.de', 80, '/', nil], parse('http://www.ntecs.de/').to_a
+    assert_equal ['www.ntecs.de', 80, '/', nil], parse('http://www.ntecs.de:80/').to_a
+
+    assert_equal ['www.ntecs.de', 80, '/', 'query'], parse('http://www.ntecs.de?query').to_a
+    assert_equal ['www.ntecs.de', 80, '/', 'query'], parse('http://www.ntecs.de/?query').to_a
+  end
+
+  def test_with_base_url
+    base = parse('http://www.ntecs.de/?query')
+    assert_equal ['www.ntecs.de', 80, '/a/b/c', nil], parse('a/b/c', base).to_a
+    assert_equal ['www.ntecs.de', 80, '/a/b/c', nil], parse('/a/b/c', base).to_a
+    assert_equal ['www.ntecs.de', 80, '/', nil], parse('', base).to_a
+
+    base = parse('http://www.ntecs.de/d/e/f?query')
+    assert_equal ['www.ntecs.de', 80, '/d/e/f', nil], parse('', base).to_a
+    assert_equal ['www.ntecs.de', 80, '/d/e/f', "query2"], parse('?query2', base).to_a
+    assert_equal ['www.ntecs.de', 80, '/abc', "query2"], parse('/abc?query2', base).to_a
+    assert_equal ['www.ntecs.de', 80, '/abc', "query2"], parse('http://www.ntecs.de/abc?query2', base).to_a
+    assert_equal ['www.ntecs.de', 80, '/d/e/gh', "query2"], parse('gh?query2', base).to_a
+  end
+
+  protected
+
+  def parse(href, base_url=nil)
+    HttpUrl.parse(href, base_url)
+  end
+end
