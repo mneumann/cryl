f6b0668f20645e43d4faf469d2240538b58e931e
Michael Neumann <mneumann@ntecs.de>
2008-03-29 01:44:04 +0100

    Refactor. Major change is how the aggregate works.

--- cryl.rb
+++ cryl.rb
@@ -1,5 +1,6 @@
 require 'fileutils'
 require 'thread'
+require 'digest/sha1'
 require 'link_fetcher/link_fetcher'
 require 'link_aggregator/link_aggregator'
 require 'link_extractor/link_extractor'
@@ -12,7 +13,6 @@ class Cryl
 
     @storage_dir = File.join(@root_dir, "files")
     @stages_dir = File.join(@root_dir, "stages")
-    @fetch_log = File.join(@root_dir, "fetch.log")
 
     FileUtils.mkdir_p(@root_dir)
     FileUtils.mkdir_p(@storage_dir)
@@ -22,75 +22,73 @@ class Cryl
     @link_aggregator = LinkAggregator.new
   end
 
-  attr_accessor :fetch_log
-
-  def bulk_fetch(io, log_file=nil)
-    LinkFetcher.new(@storage_dir, log_file) do |fetcher|
-      while line = io.gets
-        line.chomp!
-        fetcher.fetch(line)
+  def fetch(io, n, log_file=nil)
+    if n == 1
+      LinkFetcher.new(@storage_dir, log_file) do |fetcher|
+        while line = io.gets
+          line.chomp!
+          fetcher.fetch(line)
+        end
       end
+    else
+      mutex = Mutex.new
+      (1..n).map {|t|
+        Thread.new {
+          LinkFetcher.new(@storage_dir, log_file ? log_file % t : nil) do |fetcher|
+            stop = false
+            while not stop
+              mutex.synchronize do
+                if !io.eof? and line = io.gets
+                  line.chomp!
+                  fetcher.fetch(line)
+                else
+                  stop = true
+                end
+              end
+            end
+            log("#{t} ready to stop")
+          end
+          log("#{t} stopped")
+        }
+      }.each {|t| t.join}
     end
   end
 
-  def bulk_fetch_threaded(io, n, log_file=nil)
-    mutex = Mutex.new
-
-    (1..n).map {|t|
-      Thread.new {
-        LinkFetcher.new(@storage_dir, log_file ? log_file % t : nil) do |fetcher|
-          stop = false
-          while not stop
-            mutex.synchronize do
-              if !io.eof? and line = io.gets
-                line.chomp!
-                fetcher.fetch(line)
+  #
+  # Read URLs from urls_in, convert them to a path name (using
+  # digest_url),
+  #
+  def aggregate(urls_in, urls_out, urls_rej)
+    while url = urls_in.gets
+      url.chomp!
+      if digest = digest_url(url)
+        fn = File.join(@storage_dir, digest + ".data")
+        if File.exist?(fn)
+          @link_aggregator.with_base_url(url) do
+            @link_extractor.parse(fn) do |href|
+              take, nurl = @link_aggregator.check(href)
+              if take
+                urls_out.puts nurl
               else
-                stop = true
+                urls_rej.puts nurl if @keep_rejected_urls
               end
             end
           end
-          log("#{t} ready to stop")
+        else
+          log("non-existing data file: #{fn}")
         end
-        log("#{t} stopped")
-      }
-    }.each {|t| t.join}
-  end
-
-  def cycle(urls_in, urls_out, urls_rej=nil, not_before=Time.now)
-    log("fetching...")
-    if @num_threads == 1
-      bulk_fetch(urls_in, @fetch_log)
-    else
-      bulk_fetch_threaded(urls_in, @num_threads, @fetch_log + ".%s")
-    end
-
-    log("URL extracting/aggregating...")
-    Dir.glob("#{@storage_dir}/**/*.url") do |fn|
-      next if not_before and File.stat(fn).mtime < not_before
-      @link_aggregator.with_base_url(File.read(fn)) do
-        @link_extractor.parse(fn[0..-4] + "data") do |href|
-          take, url = @link_aggregator.check(href)
-          if take
-            urls_out.puts url
-          else
-            urls_rej.puts url if urls_rej
-          end
-        end
+      else
+        log("invalid URL: #{url}")
       end
     end
   end
 
   def crawl(last_url_file, max_depth=2, cur_depth=0, update=false)
     log("entered method crawl #{cur_depth}/#{max_depth}")
-    next_url_file = crawl_cycle(last_url_file, cur_depth, (update && cur_depth == 0) ? nil : Time.now)
-    crawl(next_url_file, max_depth, cur_depth+1) if cur_depth < max_depth
-  end
 
-  def crawl_cycle(last_url_file, stage, not_before=nil) 
-    urls_in_name = urls_file(stage, ".in")
-    urls_out_name = urls_file(stage, ".out")
-    urls_rej_name = urls_file(stage, ".rej")
+    urls_in_name = urls_file(cur_depth, ".in")
+    urls_out_name = urls_file(cur_depth, ".out")
+    urls_rej_name = urls_file(cur_depth, ".rej")
 
     begin
       File.link(last_url_file, urls_in_name)
@@ -98,16 +96,23 @@ class Cryl
       FileUtils.copy(last_url_file, urls_in_name)
     end
 
-    self.fetch_log = File.join(stage_n(stage), "fetch.log")
-
+    log("fetching...")
     File.open(urls_in_name, "r") do |urls_in|
-      File.open(urls_out_name, "w+") do |urls_out|
-        File.open(urls_rej_name, "w+") do |urls_rej|
-          cycle(urls_in, urls_out, @keep_rejected_urls ? urls_rej : nil, not_before)
+      fetch(urls_in, @num_threads, File.join(stage_n(cur_depth), "fetch.log.%s"))
+    end
+
+    if cur_depth < max_depth
+      log("URL extracting/aggregating...")
+      File.open(urls_in_name, "r") do |urls_in|
+        File.open(urls_out_name, "w+") do |urls_out|
+          File.open(urls_rej_name, "w+") do |urls_rej|
+            aggregate(urls_in, urls_out, urls_rej)
+          end
         end
       end
+
+      crawl(urls_out_name, max_depth, cur_depth+1)
     end
-    return urls_out_name
   end
 
   def urls_file(stage, ext=nil)
@@ -118,6 +123,23 @@ class Cryl
     n = File.join(@stages_dir, stage.to_s)
     FileUtils.mkdir_p(n)
     return n
+  end
+
+  def digest_url(url)
+    if url =~ /^http:\/\/([^\/]+)(.*)$/
+      host_port, req_uri = $1.downcase, $2
+      host, port = host_port.split(":", 2)
+      port ||= 80
+
+      hex = Digest::SHA1.hexdigest("http://#{host}:#{port}#{req_uri}")
+
+      list = host.gsub(/[^0-9a-zA-Z.-]+/, '').split(".").reverse
+      list << hex[0,2]
+      list << hex[2..-1]
+      list.join("/")
+    else
+      nil
+    end
   end
 
 end
